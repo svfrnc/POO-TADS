b336dbf85c54b69877725b226488bfdd08418b19
Sávio França <164408156+svfrnc@users.noreply.github.com>
2026-04-27 16:07:24 +0000

    questões da lista 03

---
+++ Lista 03 B/Questão_01.py
@@ -0,0 +1,7 @@
+class cliente:
+    def __init__(self, id, nome, email, fone):
+        self.__id = id
+        self.__nome = nome
+        self.__email = email
+        self._fone = fone
+        pass
