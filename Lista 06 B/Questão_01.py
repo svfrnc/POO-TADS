e0afddae78ff6f82159ea38c162213e6f69158f9
Sávio França <164408156+svfrnc@users.noreply.github.com>
2026-04-28 20:37:55 +0000

    construtor cliente

--- Lista 06 B/Questão_01.py
+++ Lista 06 B/Questão_01.py
@@ -1,8 +1,33 @@
-class cliente:
+class Cliente:
     def __init__(self, id, nome, email, fone):
         self.__id = id
         self.__nome = nome
         self.__email = email
-        self._fone = fone
+        self.__fone = fone
         pass
-    def 
+    def __str__(self):
+        return f"{self.__id} - {self.__nome} - {self.__email} - {self.__fone}"
+    def set_id(self, id):
+        if id > 0:
+            self.__id = id
+        else:
+            return ValueError("valor de ID incompatível")
+    def set_nome(self, nome):
+        self.__nome = nome
+    def set_email(self, email):
+        self.__email = email
+    def set_fone(self, nome):
+        self.__nome = nome
+    def get_id(self):
+        return id
+    def get_nome(self):
+        return self.__nome
+    def get_email(self):
+        return self.__email
+    def get_fone(self):
+        return self.__fone
+    
+    
+
+
+
